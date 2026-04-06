3ac89896253b025f53426bc0afb7a89c6cb92487
Mike Ogar <ogar.m.v@mail.ru>
2026-04-06 14:13:20 +0300

    spring-boot

--- src/main/java/org/example/controllers/AdviceController.java
+++ src/main/java/org/example/controllers/AdviceController.java
@@ -7,12 +7,21 @@ TODO
  @RestController означает, что класс будет отвечать на HTTP-запросы,
  а именно возвращать данные(текст или json).
  Как вывеска слова "магазин", чтобы люди заходили в помещение.
+ При запуске программы, когда Spring Boot стартует, он:
+    1) Видит @RestController
+    2) Понимает: "Нужно создать объект этого класса"
+    3) Создаёт объект с помощью конструктора
+    4) Кладёт его в свою "копилку" (контейнер Spring)
  */
 @RestController
 public class AdviceController {
 
     private final AdviceService adviceService;
 
+    /*
+    TODO
+    Конструктор используется при создании Spring(ом) объекта AdviceController
+     */
     public AdviceController(AdviceService adviceService) {
         this.adviceService = adviceService;
     }
